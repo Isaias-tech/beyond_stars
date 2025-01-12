6d174c45a5628bd778eb55443712daad717b5d28
Isaias-tech <isaias-santiago-toribio@outlook.com>
2025-01-12 10:23:25 -0400

    feat: add profile and is admin information to the user model

--- models/user.py
+++ models/user.py
@@ -10,12 +10,18 @@ class User(db.Model, UserMixin):
     last_name = sa.Column(sa.String(150))
     email = sa.Column(sa.String(), unique=True)
     password = sa.Column(sa.String(128))
+    phone_number = sa.Column(sa.String(21))
+    first_address = sa.Column(sa.Text())
+    second_address = sa.Column(sa.Text())
+    zip_code = sa.Column(sa.String(10))
     created_at = sa.Column(sa.DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = sa.Column(
         sa.DateTime,
         default=lambda: datetime.now(timezone.utc),
         onupdate=lambda: datetime.now(timezone.utc),
     )
+
+    is_admin = sa.Column(sa.Boolean(), default=False)
 
     def set_password(self, password):
         self.password = bcrypt.generate_password_hash(password).decode("utf-8")
